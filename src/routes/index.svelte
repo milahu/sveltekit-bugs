971d60dc378b5b302a478d7789135921c6a909d4
vwkd <33468089+vwkd@users.noreply.github.com>
2021-08-23 23:03:55 +0300

    setup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,11 @@
-<h1>Index</h1>
+<script>
+    async function loadModule() {
+        const { foo } = await import("$lib/mod.js");
+        // vvv DELETE ME vvv
+        await import("$lib/mod.css");
+        
+        console.log("Module loaded successfully.")
+    }
+</script>
+
+<button on:click={loadModule}>Load module</button>
